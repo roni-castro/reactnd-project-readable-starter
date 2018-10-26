6fe58207c3dae7625fb28b150086a6199f3d4d8b
Roni <roni.c.castro@gmail.com>
2018-10-25 23:07:31 -0200

    Add vote down and up icons with badge

--- frontend/src/components/ListCard.js
+++ frontend/src/components/ListCard.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, CardTitle, CardText } from 'reactstrap'
+import { Card, CardTitle, CardText, Container, Row, Col, Badge } from 'reactstrap'
 import * as moment from 'moment';
+import { MdThumbDown, MdThumbUp } from "react-icons/md";
 
 export const ListCard = ({posts}) => (
     <div>
         {posts.map((post) => (
-            <Card key={post.id} body outline color="secondary">
-                <CardTitle>{post.title}</CardTitle>
-                <CardText>{post.body}</CardText>
-                <CardText>
-                    <small className="text-muted">{`Created by ${post.author} 
-                        on: ${moment(post.timestamp).format("LLL")}`}
-                    </small>
-                </CardText>
-            </Card>
+            <Container key={post.id}>
+                <Row>
+                    <Col xs="auto">
+                        <MdThumbUp size={24}/>
+                        <Badge color="secondary">1</Badge>
+                        <MdThumbDown size={24}/>
+                        <Badge color="secondary">4</Badge>
+                    </Col>
+                    <Col>
+                        <Card body outline color="secondary">
+                            <CardTitle>{post.title}</CardTitle>
+                            <CardText>{post.body}</CardText>
+                            <CardText>
+                                <small className="text-muted">{`Created by ${post.author} 
+                                    on: ${moment(post.timestamp).format("LLL")}`}
+                                </small>
+                            </CardText>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
             )
         )}
     </div>
