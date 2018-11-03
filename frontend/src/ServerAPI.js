0b6e4c5e18b3480ef686269761fcb796b9c3bfe2
Roni <roni.c.castro@gmail.com>
2018-11-03 19:33:15 -0300

    Add content type to header

--- frontend/src/ServerAPI.js
+++ frontend/src/ServerAPI.js
@@ -5,6 +5,7 @@ let authorizationToken = Math.random().toString(36).substr(-8)
 
 const headers = {
   'Accept': 'application/json',
+  'Content-Type': 'application/json',
   'Authorization': authorizationToken
 }
 
@@ -14,11 +15,16 @@ export const getAll = () =>
     .then(data => data.categories)
 
 export const getAllPosts = () =>
-    fetch(`${api}/posts`, { headers })
-        .then(res => res.json())
-        .catch(err => console.log(err))
+  fetch(`${api}/posts`, { headers })
+      .then(res => res.json())
+      .catch(err => console.log(err))
 
 export const updateVote = (id, params) => 
-  fetch(`${api}/posts/${id}`, {method: 'POST', headers: headers, body: JSON.stringify({option: params})})
-    .then(res => res.json())
-    .catch(err => console.log(err))
+  fetch(`${api}/posts/${id}`, {
+    credentials: 'same-origin', 
+    method: 'POST',
+    body: JSON.stringify({option: params}),
+    headers,
+  })
+  .then(response => response.json())
+  .catch(err => console.log(err))
