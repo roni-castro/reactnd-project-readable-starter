7e8b2799134ae3734a3d4cb2a92da0079baa3752
Roni <roni.c.castro@gmail.com>
2018-11-03 21:30:24 -0300

    Allow order the posts by vote scores or timestamp

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { PostCardList } from './PostCardList';
-import * as ServerAPI from '../ServerAPI'
+import { FilterSelect } from './FilterSelect';
+import * as ServerAPI from '../ServerAPI';
+import 
+{ 
+    VOTE_FILTER_TYPE, 
+    TIMESTAMP_FILTER_TYPE
+} from '../utils/Constants'
 
 export class Home extends React.Component {
     state = {
+        filterTypes:  [
+            {id: VOTE_FILTER_TYPE, value: VOTE_FILTER_TYPE.toUpperCase()},
+            {id: TIMESTAMP_FILTER_TYPE, value: TIMESTAMP_FILTER_TYPE.toUpperCase()}
+        ],
+        filterSelectedId: VOTE_FILTER_TYPE,
         posts: []
     }
     
@@ -23,19 +34,42 @@ export class Home extends React.Component {
         })
     }
 
+    onFilterSelected = (event) => {
+        this.setState({
+            filterSelectedId: event.target.value
+        }, this.getAllPosts())
+
+    }
+
     getAllPosts() {
         ServerAPI.getAllPosts().then((posts) => {
-            console.log(posts)
+            posts = this.orderPosts(this.state.filterSelectedId, posts)
             this.setState({
                 posts
             })
         })
     }
 
+    orderPosts(filterSelectedId, posts) {
+        if(filterSelectedId === TIMESTAMP_FILTER_TYPE) {
+           return posts.sort((a,b) => new Date(a.timestamp) - new Date(b.timestamp))
+        } else if(filterSelectedId === VOTE_FILTER_TYPE) {
+           return posts.sort((a,b) => b.voteScore - a.voteScore) 
+        } else {
+            return posts
+        }
+    }
+
     render() {
         return (
             <div>
                 <h1>Home</h1>
+                
+                <FilterSelect 
+                    options={this.state.filterTypes}
+                    optionSelectedId={this.state.filterSelectedId}
+                    onFilterSelected={this.onFilterSelected} 
+                />
                 <PostCardList posts={this.state.posts} onVoteUp={this.onVoteUp} onVoteDown={this.onVoteDown}/>
             </div>
         )
---
+++ frontend/src/utils/Constants.js
@@ -0,0 +1,2 @@
+export const VOTE_FILTER_TYPE = "vote";
+export const TIMESTAMP_FILTER_TYPE = "timestamp";
